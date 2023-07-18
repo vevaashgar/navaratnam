3126c4fadac28a7f4bf60edeff60b251e158c4c7
vevaashgar <134509603+vevaashgar@users.noreply.github.com>
2023-07-18 13:43:03 +0100

    Update app.js

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Hi This is Vevaash! Add one above!</p>
+                <p className="text-center">Hi This is Vevaash! Whats up!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
